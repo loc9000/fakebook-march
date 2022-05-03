c67434cda6a4b882dcc439319fc75b9f17491f8e
derekcodingtemple <derekh@codingtemple.com>
2022-05-03 15:10:55 -0500

    added database and models

--- app/blueprints/main/routes.py
+++ app/blueprints/main/routes.py
@@ -1,10 +1,12 @@
 from flask import render_template, current_app as app
+from datetime import datetime as dt
+from app.blueprints.blog.models import Post
 
 # MAIN APPLICATION ROUTES
 @app.route('/')
 def home():
     # raise Exception('This is a general exception I\'m trying to raise for no reason.')
-    return render_template('main/home.html')
+    return render_template('main/home.html', posts=[post.to_dict() for post in Post.query.all()])
 
 @app.route('/profile')
 def profile():
@@ -13,3 +15,11 @@ def profile():
 @app.route('/contact')
 def contact():
     return "Contact Page"
+
+# OBJECT RELATIONAL MAPPER
+
+# class Post:
+#     id
+#     body
+
+# SQL - post => id, body, date_created
